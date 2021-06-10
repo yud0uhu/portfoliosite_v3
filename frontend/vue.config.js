82d665531d5c338d6c0bdb89630451efc2e08895
root <root@DESKTOP-GNI3ULF.localdomain>
2021-06-11 01:10:11 +0900

    fix vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
         plugins: [
           new PrerenderSPAPlugin({
             staticDir: path.join(__dirname, "dist"),
-            routes: ["/", "/about"],
+            routes: ["/", "/about", "/articles/:id"],
           }),
         ],
       };
