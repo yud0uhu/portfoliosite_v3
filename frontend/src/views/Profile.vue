80e5f323419cdb2727b5a971254f9ddaee7a7b1c
yud0uhu <cist.b219.y.osaka@gmail.com>
2021-08-20 03:08:29 +0900

    プロフィールページを作成

---
+++ frontend/src/views/Profile.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          About me
+        </h1>
+      </v-flex>
+            <v-flex xs12>
+        <v-img
+          :src="require('../assets/icon.jpg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex
+        mb-5
+        xs12
+      >
+        <h2 class="headline font-weight-bold mb-3">0yu</h2>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+
+        <v-layout justify-center>
+          <a
+            v-for="(link, i) in importantLinks"
+            :key="i"
+            :href="link.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ link.text }}
+          </a>
+
+        <a href="https://hackteck.hatenablog.com/"><svg role="img" viewBox="0 0 32 32" width='24' height='24' fill=#00A4DE xmlns="http://www.w3.org/2000/svg"><title>Hatena Bookmark</title><path d="M20.47 0C22.42 0 24 1.58 24 3.53v16.94c0 1.95-1.58 3.53-3.53 3.53H3.53C1.58 24 0 22.42 0 20.47V3.53C0 1.58 1.58 0 3.53 0h16.94zm-3.705 14.47c-.78 0-1.41.63-1.41 1.41s.63 1.414 1.41 1.414 1.41-.645 1.41-1.425-.63-1.41-1.41-1.41zM8.61 17.247c1.2 0 2.056-.042 2.58-.12.526-.084.976-.222 1.32-.412.45-.232.78-.564 1.02-.99s.36-.915.36-1.48c0-.78-.21-1.403-.63-1.87-.42-.48-.99-.734-1.74-.794.66-.18 1.156-.45 1.456-.81.315-.344.465-.824.465-1.424 0-.48-.103-.885-.3-1.26-.21-.36-.493-.645-.883-.87-.345-.195-.735-.315-1.215-.405-.464-.074-1.29-.12-2.474-.12H5.654v10.486H8.61zm.736-4.185c.705 0 1.185.088 1.44.262.27.18.39.495.39.93 0 .405-.135.69-.42.855-.27.18-.765.254-1.44.254H8.31v-2.297h1.05zm8.656.706v-7.06h-2.46v7.06H18zM8.925 9.08c.71 0 1.185.08 1.432.24.245.16.367.435.367.83 0 .38-.13.646-.39.804-.265.154-.747.232-1.452.232h-.57V9.08h.615z"/></svg></a>
+
+        <a href="https://zenn.dev/denham/scraps"><svg role="img" viewBox="0 0 32 32" width='24' height='24' fill=#3EA8FF xmlns="http://www.w3.org/2000/svg"><title>Zenn</title><path d="M.264 23.771h4.984c.264 0 .498-.147.645-.352L19.614.874c.176-.293-.029-.645-.381-.645h-4.72c-.235 0-.44.117-.557.323L.03 23.361c-.088.176.029.41.234.41zM17.445 23.419l6.479-10.408c.205-.323-.029-.733-.41-.733h-4.691c-.176 0-.352.088-.44.235l-6.655 10.643c-.176.264.029.616.352.616h4.779c.234-.001.468-.118.586-.353z"/></svg></a>
+
+        </v-layout>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      importantLinks: [
+        {
+          text: 'GitHub',
+          href: 'https://github.com/yud0uhu'
+        },
+        {
+          text: 'Blog',
+          href: 'https://yukimi-pino.netlify.app/about/'
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/denham95173179'
+        }
+      ]
+    })
+  }
+</script>
